832d781e804e0b47f44d0fa44bd3444aa15db966
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-24 20:45:17 +0100

    first

---
+++ models/authors.js
@@ -0,0 +1,27 @@
+const db = require("../db")();
+const COLLECTION = "authors";
+module.exports = () => {
+    const get = async (id = null) => {
+        console.log(" inside authors model");
+        if (!id) {
+            const authors = await db.get(COLLECTION);
+            return authors;
+        }
+        const author = await db.get(COLLECTION, {
+            id
+        });
+        return author;
+    };
+    const add = async (name) => {
+        const authorCount = await db.count(COLLECTION);
+        const results = await db.add(COLLECTION, {
+            id: authorCount + 1,
+            name: name,
+        });
+        return results.result;
+    };
+    return {
+        get,
+        add,
+    };
+};
---
+++ models/books.js
@@ -0,0 +1,27 @@
+const db = require('../db')();
+const COLLECTION = "books";
+module.exports = () => {
+    const get = async (id = null) => {
+        console.log(' inside books model');
+        if (!id) {
+            const books = await db.get(COLLECTION);
+            return books;
+        }
+        return {
+            error: "byId not implemented yet"
+        }
+    }
+    const add = async (name, author) => {
+        const bookCount = await db.count(COLLECTION);
+        const results = await db.add(COLLECTION, {
+            id: bookCount + 1,
+            name: name,
+            author: author
+        });
+        return results.result;
+    }
+    return {
+        get,
+        add
+    }
+};
